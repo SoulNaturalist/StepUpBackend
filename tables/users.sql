fb2469cfbfaf90a991f9b110646cdb605ae6db2c
SoulNaturalist <omg12121@mail.ru>
2023-10-01 10:06:31 +0300

    added column email_confirm in table

--- tables/users.sql
+++ tables/users.sql
@@ -2,5 +2,6 @@ CREATE TABLE users (
     userId serial primary key,
     name varchar(55),
     email varchar(255),
-    password varchar(255)
+    email_confirm BOOLEAN NOT NULL,
+    password varchar(255),
 );
